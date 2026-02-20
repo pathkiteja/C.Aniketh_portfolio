278bd25657ce5934b5b8a4bad68125c2a9bafde2
pathkiteja <pathkiteja@gmail.com>
2026-02-21 00:16:47 +0530

    cover letter added

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -81,7 +81,8 @@ const Hero = () => {
       { name: 'Resume_Aniketh_1.pdf', url: '/Resume Aniketh (1).pdf' },
       { name: 'Aniketh_Resume.pdf', url: '/Aniketh Resume.pdf' },
       { name: 'Resume_C_Aniketh.pdf', url: '/Resume C.Aniketh.pdf' },
-      { name: 'Resume.pdf', url: '/resume.pdf' }
+      { name: 'Resume.pdf', url: '/resume.pdf' },
+      { name: 'C.Aniketh Cover_Letter.pdf', url: '/C.Aniketh Cover_Letter.pdf'}
     ];
 
     // Download each resume
@@ -117,7 +118,7 @@ const Hero = () => {
           />
           
           {/* Animated gradient orbs */}
-          <div className="absolute top-1/4 left-1/4 w-72 h-72 bg-primary-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-float"></div>
+          <div className="absolute top-1/4 left-1/4 w-72 h-72 bg-primary-z400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-float"></div>
           <div className="absolute top-1/3 right-1/4 w-72 h-72 bg-accent-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-float" style={{ animationDelay: '2s' }}></div>
           <div className="absolute bottom-1/4 left-1/3 w-72 h-72 bg-purple-400 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-float" style={{ animationDelay: '4s' }}></div>
           
